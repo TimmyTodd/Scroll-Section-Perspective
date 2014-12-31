7e38c0f60372d76ee172b8cb014f6600a287c3ad
Todd Carpenter <toddbc@gmail.com>
2014-12-31 11:23:06 -0500

    have perspective working on scroll

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-var topPos = $( ".current" ).scrollTop();
+var topPos = $( ".current" ).scrollTop(),
+rotate = topPos + "deg",
+opac = 0;
 
 function init() {
-	$( "div" ).css( "height", $(window).height() );
-	$( "div.current" ).css( "top", "0" );
-	$( "div:nth-child(2)" ).css( "top", $(window).height() );
-	$( "div:nth-child(3)" ).css( "top", $(window).height() * 2 );
-	$( "div:nth-child(4)" ).css( "top", $(window).height() * 3 );
+	$( ".content" ).css({ "height" : $(window).height(), "width" : $(window).width() });
+
+	$( ".content:nth-child(1)" ).css( "top", "0" );
+	$( ".content:nth-child(2)" ).css( "top", $(window).height() );
+	$( ".content:nth-child(3)" ).css( "top", $(window).height() * 2 );
+	$( ".content:nth-child(4)" ).css( "top", $(window).height() * 3 );
 };
 
 $( document ).ready(function() {
@@ -24,13 +27,24 @@ $(window).scroll(function() {
     
     topPos = Math.round( windowTop / $(window).height() * 90 ); // get percentage of top position vs current window height
 
+    opacTopPos = Math.round(windowTop / $(window).height() * 10) / 10; // get percentage of top position vs current window height
+
     /*var scrol_pos = $(window).scrollTop();
     console.log( scrol_pos );*/
 
     /*$( ".current" ).css( "margin-top", windowTop / 2 + "px");
     $( ".current" ).css( "margin-bottom", "-" + windowTop / 2 + "px");*/
 
-    var rotate = "rotateX( -" + topPos + "deg)";
-    $( ".current" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
+    if ( topPos <= 90 ) {
+    	rotate = "rotateX( -" + topPos + "deg)";
+    	$( ".current .inner" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
+
+    	opac = opacTopPos;
+    	$( ".current .grad" ).css({"opacity" : opac});
+    } else {
+    	rotate = "rotateX( -90deg)";
+    	$( ".current .inner" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
+    	opac = 1;
+    }
 
 });
