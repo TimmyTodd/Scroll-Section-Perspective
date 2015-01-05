b01a2da69ed0eedcee1abe98d4a46953fa19412a
Todd Carpenter <toddbc@gmail.com>
2015-01-05 11:25:03 -0500

    smoother scrolling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var topPos = $( ".current" ).scrollTop(),
 rotate = topPos + "deg",
-opac = 0;
+opac = 0,
+lastScrollTop = 0;
 
 function init() {
 	$( ".content" ).css({ "height" : $(window).height(), "width" : $(window).width() });
@@ -22,10 +23,12 @@ $( window ).resize(function() {
 
 $(window).scroll(function() {
 
+    var st = $(this).scrollTop();
+
 	/*var $testElem = document.querySelector( "div:nth-child(3)" );
 	console.log( $testElem.dataset.view );*/
 
-	windowTop = $(window).scrollTop();
+	windowTop = st;
 	console.log(windowTop);
     
     topPos = Math.round( windowTop / $(window).height() * 90 ); // get percentage of top position vs current window height
@@ -38,18 +41,31 @@ $(window).scroll(function() {
     /*$( ".current" ).css( "margin-top", windowTop / 2 + "px");
     $( ".current" ).css( "margin-bottom", "-" + windowTop / 2 + "px");*/
 
-    if ( topPos <= 90 ) {
+    if ( topPos <= 60 ) {
     	rotate = "rotateX( -" + topPos + "deg)";
     	$( ".current .inner" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
 
     	opac = opacTopPos;
     	$( ".current .grad" ).css({"opacity" : opac});
-    } else {
+    }
+    else if ( st > lastScrollTop && topPos > 20 && topPos < 90 ) {
+        /*$("html, body").animate({ scrollTop: $(document).height() }, 300, function() {
+            lastScrollTop = st
+        });*/
+
+
+        rotate = "rotateX( -90deg)";
+        $( ".current .inner" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
+        opac = 1;
+    }
+    else {
     	rotate = "rotateX( -90deg)";
     	$( ".current .inner" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
     	opac = 1;
     }
 
     $( ".content" ).css({ "height" : $(window).height(), "width" : $(window).width() });
+
+    lastScrollTop = st;
 
 });
