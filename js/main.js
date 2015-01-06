ddc6f3178655c01784158bd67f6da229e1b6495c
Todd Carpenter <toddbc@gmail.com>
2015-01-06 09:46:31 -0500

    scroll fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var topPos = $( ".current" ).scrollTop(),
 rotate = topPos + "deg",
 opac = 0,
-lastScrollTop = 0;
+lastScrollTop = 0,
+stopScroll = 0;
 
 function init() {
 	$( ".content" ).css({ "height" : $(window).height(), "width" : $(window).width() });
@@ -29,7 +30,7 @@ $(window).scroll(function() {
 	console.log( $testElem.dataset.view );*/
 
 	windowTop = st;
-	console.log(windowTop);
+	//console.log(windowTop);
     
     topPos = Math.round( windowTop / $(window).height() * 90 ); // get percentage of top position vs current window height
 
@@ -41,27 +42,42 @@ $(window).scroll(function() {
     /*$( ".current" ).css( "margin-top", windowTop / 2 + "px");
     $( ".current" ).css( "margin-bottom", "-" + windowTop / 2 + "px");*/
 
-    if ( topPos <= 60 ) {
-    	rotate = "rotateX( -" + topPos + "deg)";
+    if ( lastScrollTop > st) {
+        console.log("last: "+lastScrollTop+" current: "+st)
+    }
+    else if ( stopScroll == 0 && st > 30 ) {
+
+        
+    	/*rotate = "rotateX( -" + topPos + "deg)";
     	$( ".current .inner" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
 
     	opac = opacTopPos;
-    	$( ".current .grad" ).css({"opacity" : opac});
-    }
-    else if ( st > lastScrollTop && topPos > 20 && topPos < 90 ) {
-        /*$("html, body").animate({ scrollTop: $(document).height() }, 300, function() {
-            lastScrollTop = st
-        });*/
+    	$( ".current .grad" ).css({"opacity" : opac});*/
+
+
+        $( ".content:nth-child(2)" ).animate({ 
+            "top": "0px"
+        }, 800, function() {
+            // after
+        })
 
 
         rotate = "rotateX( -90deg)";
-        $( ".current .inner" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
-        opac = 1;
-    }
-    else {
+        $( ".current .inner" ).animate({ 
+            "-moz-transform" : rotate,
+            "-webkit-transform" : rotate
+        }, 1300, function() {
+            // after
+        })
+
+        stopScroll = 1
+
+    } /*else if () {
     	rotate = "rotateX( -90deg)";
     	$( ".current .inner" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
     	opac = 1;
+    }*/ else {
+
     }
 
     $( ".content" ).css({ "height" : $(window).height(), "width" : $(window).width() });
