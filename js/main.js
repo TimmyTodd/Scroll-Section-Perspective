3466dc40b3e0c1353d3a9ef7099e534648cdfe3b
Todd Carpenter <toddbc@gmail.com>
2015-01-06 10:30:06 -0500

    removed scrolling. using button instead

--- js/main.js
+++ js/main.js
@@ -5,36 +5,50 @@ lastScrollTop = 0,
 stopScroll = 0;
 
 function init() {
-	$( ".content" ).css({ "height" : $(window).height(), "width" : $(window).width() });
+	$( "body,.content" ).css({ "height" : $(window).height(), "width" : $(window).width() });
 
 	$( ".content:nth-child(1)" ).css( "top", "0" );
-	$( ".content:nth-child(2)" ).css( "top", $(window).height() );
-	//$( ".content:nth-child(3)" ).css( "top", $(window).height() * 2 );
-	//$( ".content:nth-child(4)" ).css( "top", $(window).height() * 3 );
-};
+    
+    if ( stopScroll == 0 ) {
+	   $( ".content:nth-child(2)" ).css( "top", $(window).height() );
+    } else {
+    }
+}
+
+function AnimateRotate(angle) {
+
+    // caching the object for performance reasons
+    var $elem = $( ".current .inner" );
+
+    // we use a pseudo object for the animation
+    // (starts from `0` to `angle`), you can name it as you want
+    $({deg: 0}).animate({deg: angle}, {
+        duration: 400,
+        step: function(now) {
+            // in the step-callback (that is fired each step of the animation),
+            // you can use the `now` paramter which contains the current
+            // animation-position (`0` up to `angle`)
+            $elem.css({
+                transform: 'rotateX(-' + now + 'deg)'
+            });
+        }
+    });
+}
 
 $( document ).ready(function() {
 	init()
-});
+})
+
 $( window ).resize(function() {
 	init()
 }).load(function() {
 	init()
-});
+})
 
-$(window).scroll(function() {
-
-    var st = $(this).scrollTop();
+$("button").click(function() {
 
 	/*var $testElem = document.querySelector( "div:nth-child(3)" );
 	console.log( $testElem.dataset.view );*/
-
-	windowTop = st;
-	//console.log(windowTop);
-    
-    topPos = Math.round( windowTop / $(window).height() * 90 ); // get percentage of top position vs current window height
-
-    opacTopPos = Math.round(windowTop / $(window).height() * 10) / 10; // get percentage of top position vs current window height
 
     /*var scrol_pos = $(window).scrollTop();
     console.log( scrol_pos );*/
@@ -42,11 +56,7 @@ $(window).scroll(function() {
     /*$( ".current" ).css( "margin-top", windowTop / 2 + "px");
     $( ".current" ).css( "margin-bottom", "-" + windowTop / 2 + "px");*/
 
-    if ( lastScrollTop > st) {
-        console.log("last: "+lastScrollTop+" current: "+st)
-    }
-    else if ( stopScroll == 0 && st > 30 ) {
-
+    if ( stopScroll == 0 ) {
         
     	/*rotate = "rotateX( -" + topPos + "deg)";
     	$( ".current .inner" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
@@ -55,33 +65,39 @@ $(window).scroll(function() {
     	$( ".current .grad" ).css({"opacity" : opac});*/
 
 
-        $( ".content:nth-child(2)" ).animate({ 
+        /*$( ".content:nth-child(2)" ).animate({ 
             "top": "0px"
         }, 800, function() {
             // after
-        })
+        })*/
 
+        AnimateRotate(90)
+        $( ".current .grad" ).fadeIn(400)
 
-        rotate = "rotateX( -90deg)";
-        $( ".current .inner" ).animate({ 
-            "-moz-transform" : rotate,
-            "-webkit-transform" : rotate
-        }, 1300, function() {
+        $( ".content:nth-child(2)" ).animate({ 
+            "top": "10%"
+        }, 400, function() {
             // after
         })
 
         stopScroll = 1
 
-    } /*else if () {
-    	rotate = "rotateX( -90deg)";
-    	$( ".current .inner" ).css({"-moz-transform" : rotate, "-webkit-transform" : rotate});
-    	opac = 1;
-    }*/ else {
+    } else if ( stopScroll == 1 ) {
 
+        AnimateRotate(0)
+        $( ".current .grad" ).fadeOut(400)
+
+        $( ".content:nth-child(2)" ).animate({ 
+            "top": $(window).height()
+        }, 400, function() {
+            // after
+        })
+
+        stopScroll = 0
+
+    } else {
     }
 
     $( ".content" ).css({ "height" : $(window).height(), "width" : $(window).width() });
-
-    lastScrollTop = st;
 
 });
